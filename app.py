451b1403cbced3f984cf965b5470b34f0a11812e
Veds22 <vedant22srivastava@gmail.com>
2026-02-12 12:40:44 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import pad_sequences
+
+model = load_model('next_word_lstm.h5')
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+    
+# Function to predict the next word
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+st.title('Next word prediction with lstm')
+input_text = st.text_input('enter the sequences of word', "To be or not to be")
+if st.button('Predict'):
+    max_sequnce_len = model.input_shape[1] + 1
+    next_word = predict_next_word(model, tokenizer, input_text, max_sequnce_len)
+    st.write(f'Next word: {next_word}')
+    
